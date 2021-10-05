5be9f2275c164f54352d97f068d644af67be3253
GuillaumeDochy <guillaume.dochy@student.ehb.be>
2021-10-05 12:50:30 +0200

    Rock Paper scissors

--- Session_1/rock_paper-scissors/src/main/kotlin/Main.kt
+++ Session_1/rock_paper-scissors/src/main/kotlin/Main.kt
@@ -1,22 +1,21 @@
 fun main(){
-    val options = arrayOf<String>("rock", "paper", "scissors")
+    val options = arrayOf("rock", "paper", "scissors")
     val computerChoice = options.random()
-    //println(computerChoice)
+
     print("Pick rock, paper or scissors: ")
     val playerOption = readLine()
+
     if(computerChoice == playerOption){
         println("It's a tie!")
-    } else if (computerChoice == "rock" || playerOption == "paper"){
-        println("You won!")
-    } else if (computerChoice == "rock" || playerOption == "scissors"){
-        println("You loser!")
-    }else if (computerChoice == "paper" || playerOption == "scissors") {
-        println("You won!")
-    }else if (computerChoice == "scissors" || playerOption == "paper") {
-        println("You loser!")
-    }else if (computerChoice == "paper" || playerOption == "rock"){
-        println("You loser!")
-    }else if (computerChoice == "scissors" || playerOption == "rock") {
-        println("You won!")
+        return
+    }
+
+    val winMessage = "you won!"
+    val loseMessage = "you loser!"
+
+    when(computerChoice) {
+        "rock" -> if (playerOption == "paper") { println(winMessage) } else { println(loseMessage) }
+        "paper" -> if (playerOption == "scissors") { println(winMessage) } else { println(loseMessage) }
+        "scissors" -> if (playerOption == "rock") { println(winMessage) } else { println(loseMessage) }
     }
 }
