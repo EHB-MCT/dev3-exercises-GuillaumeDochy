1483123e8c3ef37a2e3758c0402dcf2d6ee86b3c
GuillaumeDochy <guillaume.dochy@student.ehb.be>
2021-10-12 10:54:29 +0200

    Challenge One Done
    Challenge One finished and no bugs

--- Session_2/src/main/kotlin/Main.kt
+++ Session_2/src/main/kotlin/Main.kt
@@ -1,17 +1,27 @@
 fun main () {
-    printIn("When you play the game of thrones, you win, or you die!")
+    println("When you play the game of thrones, you win, or you die!")
     challengeOne()
 }
 
 fun challengeOne(){
-    printIn("Solve this riddle to pass the first test, pick your answer carefully so your mind can rest!")
-    printIn("Winter they fear, Red they worship, with fire and ice they shall rise and fall, which of the great houses of westeros are concerned?")
+    println("Solve this riddle to pass the first test, pick your answer carefully so your mind can rest!")
+    println("Winter they fear, Red they worship, with fire and ice they shall rise and fall, which of the great houses of Westeros are concerned?")
 
     val housesOne = setOf("lannister", "targaryen")
 
     val playerChoiceOne = readLine()
 
-    If (playerChoiceOne){
-        printIn("Good job!")
+    if (housesOne.contains(playerChoiceOne)){
+        succes()
+    } else{
+        gameOver()
     }
+}
+
+fun succes(){
+    println("Good job!")
+}
+
+fun gameOver(){
+    println("nope, you failed, cut this bastards head off")
 }
