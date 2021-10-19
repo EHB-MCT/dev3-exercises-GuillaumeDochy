3cf1ee552a943a1edf693cb4b47443b1dc0320f5
GuillaumeDochy <guillaume.dochy@student.ehb.be>
2021-10-19 09:58:33 +0200

    Task 1 modified
    Modified the structure

--- Session-3/src/main/kotlin/Course.kt
+++ Session-3/src/main/kotlin/Course.kt
@@ -1,2 +1,11 @@
-class Course(val name: String, val description: String, val semester: String, val tutor: String?) {
+class Course(
+    val name: String,
+    val description: String,
+    val semester: Int,
+    val tutor: String,
+    val year: Int,
+    val credits: Int,
+    val module: String,
+    val required: Boolean
+    ) {
 }
