1fe7c22bb1843bf6bc545979c39d5bf2bd536d42
GuillaumeDochy <guillaume.dochy@student.ehb.be>
2021-11-23 12:36:01 +0100

    Update main.kt
    added a better text as answer. Search for a ride works

--- Session-6/src/main/kotlin/main.kt
+++ Session-6/src/main/kotlin/main.kt
@@ -16,6 +16,17 @@ fun main(){
                 credentials.databaseName,
         connectionProps)
 
+    var inputCity = readLine()
+
+    val statement = connection.prepareStatement("SELECT * FROM rides WHERE destination = '$inputCity' ORDER BY departure_time ASC LIMIT 1")
+    val result = statement.executeQuery()
+
+    while (result.next()){
+        var id = result.getInt("id")
+        var departure_city = result.getString("departure_city")
+        var destination = result.getString("destination")
+        var departure_time = result.getString("departure_time")
+
+        println("The next train from " +departure_city+ " to " +destination+ " departs at " +departure_time)
+    }
 }
-
-
