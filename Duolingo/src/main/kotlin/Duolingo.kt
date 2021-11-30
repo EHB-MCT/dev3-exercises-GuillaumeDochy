24d41fbda9c3d99ee3cfb22fbdbd9b707e814585
GuillaumeDochy <guillaume.dochy@student.ehb.be>
2021-11-30 12:28:54 +0100

    Update Duolingo.kt
    Updated play function and started constructors

--- Duolingo/src/main/kotlin/Duolingo.kt
+++ Duolingo/src/main/kotlin/Duolingo.kt
@@ -1,5 +1,10 @@
-class Duolingo {
-    val wordsList = mutableListOf<Word>(
+class Duolingo{
+    constructor(
+        roundSize: Int = 5,
+        language: String = "english"
+    )
+
+    val words = mutableListOf<Word>(
         Word("candle", "kaars", "english"),
         Word("bougie", "kaars", "french"),
 
@@ -32,59 +37,23 @@ class Duolingo {
     )
 
     fun play(){
-        println("Translate the following words in Dutch:")
 
-        val numberOfWords = 5
-        val randomElements = wordsList.asSequence().shuffled().take(numberOfWords).toList()
+        val numberOfWords = 5;
+        val randomWord = words.shuffled().take(numberOfWords).toMutableSet()
 
-        val element0 = randomElements[0]
-        val element1 = randomElements[1]
-        val element2 = randomElements[2]
-        val element3 = randomElements[3]
-        val element4 = randomElements[4]
 
-        println(element0.original + ", " + element1.original + ", " + element2.original + ", " + element3.original + ", " + element4.original)
+        while (randomWord.isNotEmpty()){
+            val selectedWord = randomWord.random()
+            println("What is the translation of ${selectedWord.original}")
+            val userAnswer = readLine()
 
-        println("Type your answer under here (1 word at a time), you have 5 words left to translate:")
-
-        val input0 = readLine()
-
-        if(input0 == element0.translated){
-            println("Correct! 4 words left, now on to the next one:")
-        }else if (input0 != element0.translated){
-            println("Wrong you idiot! The answer was: ${element0.translated}. 4 words left you faggot:")
-        }
-
-        val  input1 = readLine()
-
-        if(input1 == element1.translated){
-            println("Correct! 3 words left, now on to the next one:")
-        }else if (input1 != element1.translated){
-            println("Wrong you idiot! The answer was ${element1.translated}, 3 words left you faggot:")
-        }
-
-        val  input2 = readLine()
-
-        if(input2 == element2.translated){
-            println("Correct! 2 words left, now on to the next one:")
-        }else if (input2 != element2.translated){
-            println("Wrong you idiot! The answer was ${element2.translated}, 2 words left you faggot:")
-        }
-
-        val  input3 = readLine()
-
-        if(input3 == element3.translated){
-            println("Correct! 1 word left, now on to the next one:")
-        }else if (input3 != element3.translated){
-            println("Wrong you idiot! The answer was ${element3.translated}, 1 word left you faggot:")
-        }
-
-        val  input4 = readLine()
-
-        if(input4 == element4.translated){
-            println("Correct! That's it for today!, good job!:")
-        }else if (input4 != element4.translated){
-            println("Wrong you idiot! The answer was ${element4.translated}! No words for you anymore, stupid human!")
+            if (selectedWord.translated == userAnswer) {
+                println("Good job !")
+                randomWord.remove(selectedWord)
+            }else{
+                println("False answer. Try again later")
+            }
+            println("They are currently " + randomWord.count() + "words left")
         }
     }
 }
